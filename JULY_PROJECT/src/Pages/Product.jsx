42cf5632040af7f3f2aa7eeccd70bad326cfd95a
sunil kumar <skkumarkumawatsunil1997@gmail.com>
2024-07-22 17:43:46 +0530

    updated code

--- JULY_PROJECT/src/Pages/Product.jsx
+++ JULY_PROJECT/src/Pages/Product.jsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import axios from 'axios'
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ProgressSpinner } from "primereact/progressspinner";
+
 
 const Product = () => {
+    const[loading, setLoading] = useState(false)
     const [data, setData] = useState([]);
     const notify = () =>
         toast.success("Successfully Added In cart", {
@@ -29,11 +32,14 @@ const Product = () => {
 
     const getData = async () => {
         try {
+            setLoading(true);
             let apidata = await axios.get(
                 "https://renderjsondata.onrender.com/men"
             );
             setData(apidata.data);
             console.log(apidata.data);
+            setLoading(false)
+
         } catch (error) {
             console.log(error);
         }
@@ -57,33 +63,39 @@ const Product = () => {
     return (
         <div className="box">
             <ToastContainer />
-            <div className=" grid w-100  ">
-                {data.map((ele) => {
-                    return (
-                        <div className=" px-2 card p-2 mx-auto griddiv ">
-                            <img src={ele.image} alt="" />
-                            <p className="pt-4">{`${
-                                ele.name ? ele.name.slice(1, 25) : null
-                            } ......`}</p>
-                            <p> $ {ele.price ? ele.price : "99.99"}</p>
-                            <p>{ele.rating}</p>
-                            <div className="d-flex gap-2  ">
-                                <button
-                                    onClick={() => addtocart(ele)}
-                                    className="btn btn-secondary ">
-                                    Add To Cart
-                                </button>
-                                <button className="btn btn-warning">
-                                    Add To Favourite
-                                </button>
-                                {/* <button onClick={() => deldata(ele.id)}>
+            {loading ? (
+                <div className="text-center">
+                    <ProgressSpinner />
+                </div>
+            ) : (
+                <div className=" grid w-100  ">
+                    {data.map((ele) => {
+                        return (
+                            <div className=" px-2 card p-2 mx-auto griddiv ">
+                                <img src={ele.image} alt="" />
+                                <p className="pt-4">{`${
+                                    ele.name ? ele.name.slice(1, 25) : null
+                                } ......`}</p>
+                                <p> $ {ele.price ? ele.price : "99.99"}</p>
+                                <p>{ele.rating}</p>
+                                <div className="d-flex gap-2  ">
+                                    <button
+                                        onClick={() => addtocart(ele)}
+                                        className="btn btn-secondary ">
+                                        Add To Cart
+                                    </button>
+                                    <button className="btn btn-warning">
+                                        Add To Favourite
+                                    </button>
+                                    {/* <button onClick={() => deldata(ele.id)}>
                                     delete
                                 </button> */}
+                                </div>
                             </div>
-                        </div>
-                    );
-                })}
-            </div>
+                        );
+                    })}
+                </div>
+            )}
         </div>
     );
 };
